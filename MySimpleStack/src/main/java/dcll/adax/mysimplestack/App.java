48689f93cdcbc842fedc52306263cbb5f6e9d5ba
DRAXARD <draxard@gmx.fr>
2014-02-19 17:33:20 +0100

    First Commit
    Premier commit du projet

---
+++ MySimpleStack/src/main/java/dcll/adax/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package dcll.adax.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
